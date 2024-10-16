7711e0ff7b62094201bc6bbbc37693593dfff051
ANYuu531 <116066229+ANYuu531@users.noreply.github.com>
2024-10-16 11:19:12 +0800

    Initial Commit

---
+++ HW-2/HW_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_2App.swift
+//  HW-2
+//
+//  Created by 李安喻 on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct HW_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HW-2Tests/HW_2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HW_2Tests.swift
+//  HW-2Tests
+//
+//  Created by 李安喻 on 2024/10/16.
+//
+
+import Testing
+
+struct HW_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
